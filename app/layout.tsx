a7f3a901fc5d643c8c1476020ed2cacc740b24d5
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2024-07-15 18:30:48 -0300

    update layout.esx and page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import MainNavbar from "./components/MainNavbar";
-import Footer from "./components/Footer";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,9 +22,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={inter.className}>
-        <MainNavbar />
         {children}
-        <Footer />
       </body>
     </html>
   );
