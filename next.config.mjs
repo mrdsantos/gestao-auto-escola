5b57d13c06430f65acfb91c9db6ed1d888c44fde
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2024-07-17 19:07:13 -0300

    add lucida icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})({
+});
 
 export default nextConfig;
