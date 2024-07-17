c5877e3c1f4ed2e127100010cfaff8c8051e1e63
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2024-07-17 19:10:52 -0300

    update footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
-    <footer className="co bg-primary text-primary-content p-5">
-      <div className="w-4/6 mx-auto">
-        <p className="font-bold text-center">
+    <footer className="bg-primary p-5">
+      <div className="w-4/6 mx-auto flex flex-col flex-nowrap items-center text-center">
+        <p className="font-bold prose">
           Gestão Auto Escola
             <br />
             A gestão que sua auto escola precisa. Um sistema completo com site institucional e área administrativa para sua auto escola</p>
 
-          <p className="text-center">Copyright © ${new Date().getFullYear()} - Todos os direitos reservados</p>
+          <p className="prose">Copyright © ${new Date().getFullYear()} - Todos os direitos reservados</p>
       </div>
     </footer>
   )
