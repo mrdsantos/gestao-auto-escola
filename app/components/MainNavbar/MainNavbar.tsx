d7336cd12b4bebfd77e1f5eeb2d60e75b3c04943
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2024-07-19 11:36:24 -0300

    update menu itens

--- app/components/MainNavbar/MainNavbar.tsx
+++ app/components/MainNavbar/MainNavbar.tsx
@@ -3,6 +3,8 @@
 import { usePathname } from "next/navigation" // Importa o hook usePathname do Next.js para obter a rota atual.
 import Link from "next/link" // Importa o componente Link do Next.js para navegação.
 import clsx from "clsx" // Importa a biblioteca clsx para unir classes condicionalmente.
+import { Menu } from "lucide-react"
+
 
 
 const MainNavbar = () => {
@@ -12,9 +14,9 @@ const MainNavbar = () => {
   // Se o href corresponder à rota atual, adiciona classes extras para estilizar o link ativo.
   const linkClass = (href: string) =>
     clsx(
-      "px-3 hover:underline hover:underline-offset-8 hover:decoration-2 hover:decoration-primary", // Classes base para todos os links.
+      "px-3 hover:underline hover:underline-offset-8 hover:decoration-2 hover:decoration-primary font-semibold text-lg ", // Classes base para todos os links.
       {
-        "px-3 underline underline-offset-8 decoration-2 decoration-primary": pathname === href, // Classes adicionais se o link for o ativo.
+        "underline underline-offset-8 decoration-2 decoration-primary": pathname === href, // Classes adicionais se o link for o ativo.
       }
     );
 
@@ -25,25 +27,25 @@ const MainNavbar = () => {
         <Link href={"/"}>
         </Link>
       </div>
-      
+
       {/* Link para a página inicial */}
       <div className={linkClass("/")}>
         <Link href={"/"}>Inicio</Link>
       </div>
-      
-      {/* Link para a página Sobre */}
-      <div className={linkClass("/sobre")}>
-        <Link href={"/sobre"}>Sobre</Link>
-      </div>
-      
+
       {/* Link para a página Perguntas Frequentes */}
       <div className={linkClass("/perguntas-frequentes")}>
         <Link href={"/perguntas-frequentes"}>Perguntas Frequentes</Link>
       </div>
-      
+
+      {/* Link para a página Cadastro */}
+      <div className={linkClass("/cadastro")}>
+        <Link href={"/sobre"}>Cadastro</Link>
+      </div>
+
       {/* Link para a página de Login */}
-      <div className={linkClass("/login")}>
-        <Link href={"/login"}>Login</Link>
+      <div className={`${linkClass("/login")} bg-neutral `}>
+        <Link href={"/login"} className="inline-flex gap-1"><Menu /></Link>
       </div>
     </nav>
   )
