661e97a73aec9b39a261ea7917bbf0ecf1c8705f
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2024-07-22 17:50:34 -0300

    update item cadastro path

--- app/components/MainNavbar/MainNavbar.tsx
+++ app/components/MainNavbar/MainNavbar.tsx
@@ -1,15 +1,10 @@
 "use client" // Declara que este módulo é um módulo do cliente, permitindo a execução no cliente.
-
 import { usePathname } from "next/navigation" // Importa o hook usePathname do Next.js para obter a rota atual.
 import Link from "next/link" // Importa o componente Link do Next.js para navegação.
 import clsx from "clsx" // Importa a biblioteca clsx para unir classes condicionalmente.
 import { CircleUser, LogIn } from "lucide-react"
-
-
-
 const MainNavbar = () => {
   const pathname = usePathname(); // Obtém a rota atual usando o hook usePathname.
-
   // Função para gerar classes CSS para links de navegação.
   // Se o href corresponder à rota atual, adiciona classes extras para estilizar o link ativo.
   const linkClass = (href: string) =>
@@ -19,7 +14,6 @@ const MainNavbar = () => {
         "underline underline-offset-8 decoration-2 decoration-primary": pathname === href, // Classes adicionais se o link for o ativo.
       }
     );
-
   return (
     <nav className="navbar bg-neutral w-full md:px-10">
       {/* Container flexível contendo o ícone da casa */}
@@ -27,28 +21,22 @@ const MainNavbar = () => {
         <Link href={"/"}>
         </Link>
       </div>
-
       {/* Link para a página inicial */}
       <div className={linkClass("/")}>
         <Link href={"/"}>Inicio</Link>
       </div>
-
       {/* Link para a página Sobre */}
       <div className={linkClass("/sobre")}>
         <Link href={"/sobre"}>Sobre</Link>
       </div>
-
-
       {/* Link para a página Perguntas Frequentes */}
       <div className={linkClass("/perguntas-frequentes")}>
         <Link href={"/perguntas-frequentes"}>Perguntas Frequentes</Link>
       </div>
-
       {/* Link para a página Cadastro */}
       <div className={linkClass("/cadastro")}>
-        <Link href={"/sobre"}>Cadastro</Link>
+        <Link href={"/cadastro"}>Cadastro</Link>
       </div>
-
       {/* Link para a página de Login */}
       <div className={`${linkClass("/login")} bg-neutral `}>
         <Link href={"/login"} className="inline-flex gap-1"><CircleUser /><LogIn /></Link>
@@ -56,5 +44,4 @@ const MainNavbar = () => {
     </nav>
   )
 }
-
 export default MainNavbar // Exporta o componente MainNavbar
