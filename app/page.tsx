404da54d7cf3d7b202371d9253eac1d401f0705c
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2024-07-22 17:52:34 -0300

    add flex grow to <main>

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
-
-    <main>
+    <main className="flex-grow">
+      {/* //STYLE create margins and padding */}
       <div className="container mx-auto max-w-full flex flex-row flex-nowrap justify-evenly">
+        {/* //IMG create images for hero section */}
         <section className="w-80 h-80 border-2 border-black">
           01
         </section>
@@ -12,8 +12,8 @@ export default function Home() {
           02
         </section>
       </div>
-
       <div className="container max-w-7xl mx-auto flex flex-row flex-wrap justify-center gap-4">
+        {/* //CONTENT Create Images and content for cards */}
         <article className="card card-compact w-48 bg-base-100 shadow-xl">
           <div className="card-body">
             <h2 className="card-title">
@@ -83,8 +83,8 @@ export default function Home() {
           </figure>
         </article>
       </div>
-
       <div className="container mx-auto max-w-full flex flex-row flex-nowrap justify-evenly">
+        {/* //CONTENT Create image for section */}
         <section className="w-80 h-80 border-2 border-black">
           01
         </section>
@@ -92,7 +92,6 @@ export default function Home() {
           02
         </section>
       </div>
-
       <div className="containter flex flex-row flex-wrap justify-center content-center items-center gap-4 mx-auto max-w-full bg-primary h-60 ">
         <div className="w-3/12">
           <h1 className="prose-h1">Lorem ipsum dolor sit amet.</h1>
