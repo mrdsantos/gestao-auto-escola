ad9ab5696a2665c8abd7ce5cd28a74b32dacf060
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2024-07-30 04:58:53 -0300

    update theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,39 +6,53 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
   plugins: [
     require("@tailwindcss/typography"),
     require('daisyui')
   ],
-  daisyui: {
-    themes: [
-      {
-        gestao: {
-          "primary": "#FF6600",
-          "secondary": "#009AFF",
-          "accent": "#7C3D9A",
-          "neutral": "#E0E0E0",
-          "background": "#F7F7F7",
-        },
-
-        gestaodark: {
-          "primary": "#FF6600",
-          "secondary": "#009AFF",
-          "accent": "#7C3D9A",
-          "neutral": "#333333",
-        },
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#FF5724",
+        "primary-darker": "#F93A00",
+        "primary-lighter": "#FF7B53",
+        "complementary": "#00B3FF",
+        "complementary-darker": "#0097EE",
+        "complementary-lighter": "#B1E4FF",
+        "secondary": "#FF245E",
+        "secondary-darker": "#D81859",
+        "secondary-lighter": "#FF93B0",
+        "accent": "#FF245E",
+        "accent-darker": "#D81859",
+        "accent-lighter": "#FF93B0",
+        "background": "#F5F5F5",
+        "surface": "#EBEBEB",
       },
-    ],
-    darkTheme: "gestaodark"
+      backgroundImage: {
+        "gradient-circle": "url(/img/background-gradient-circle-1280-480.png)",
+        "gradient-square": "url(/img/background-gradient-squared-1300-400.png)"
+      },
+      screens: {
+        'sm': '639.98px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '767.98px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1023.98px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1279.98px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1535.98px',
+        // => @media (min-width: 1536px) { ... }
+      }
+    },
+  },
+  daisyui: {
+    themes: ["light", "dark"],
   },
 };
+
 export default config;
