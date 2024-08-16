8163d706a2bec7d427129e812ad15fc30cea5f1a
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2024-08-16 13:14:29 -0300

    update text

--- app/components/Infobanner/Infobanner.tsx
+++ app/components/Infobanner/Infobanner.tsx
@@ -1,7 +1,7 @@
 const InfoBanner = () => {
   return (
     <div className="bg-gradient-to-r from-[#642B73] from-60% to-[#C6426E] text-white px-5 py-1 text-sm font-semibold">
-      <p>Este site contém informações fictícias, pois o negócio ainda está em desenvolvimento.</p>
+      <p>Este site contém informações fictícias, pois o serviço ainda está em desenvolvimento.</p>
     </div>
   );
 };
