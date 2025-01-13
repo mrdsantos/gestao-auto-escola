8a47473e108989f02403495e572bd81405914287
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2025-01-13 14:31:37 -0300

    adapt the layout for website or dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,40 @@
 import type { Metadata } from "next";
 import { Quicksand } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import ThemeInitializer from "./components/ThemeInitializer/ThemeInitializer";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import InfoBanner from "./components/Infobanner/Infobanner";
+import LayoutSwitcher from "./components/LayoutSwitcher/Layoutswitcher";
 
 const quicksand = Quicksand({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
 });
+
 export const metadata: Metadata = {
   title: {
     template: "Gestão Auto Escola | %s",
-    default: "Gestão Auto Escola"
+    default: "Gestão Auto Escola",
   },
-  description: "A gestão que sua auto escola precisa. Um sistema completo com site institucional e área administrativa para sua auto escola",
-  authors: [{ name: 'Maicon Rodrigues dos Santos', url: 'https://www.linkedin.com/in/maiconrdsantos/' }],
+  description:
+    "A gestão que sua auto escola precisa. Um sistema completo com site institucional e área administrativa para sua auto escola",
+  authors: [
+    {
+      name: "Maicon Rodrigues dos Santos",
+      url: "https://www.linkedin.com/in/maiconrdsantos/",
+    },
+  ],
 };
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="pt-br">
-      <body className={`bg-background, ${quicksand.className}`}>
-      <ThemeInitializer />
-        <div className="min-h-screen flex flex-col">
-          <InfoBanner />
-          <Header />
-          {children}
-          <Footer />
-          <SpeedInsights/>
-        </div>
+      <body className={`${quicksand.className}`}>
+        <ThemeInitializer />
+        {/* Usa o LayoutSwitcher para gerenciar o layout com base na rota */}
+        <LayoutSwitcher>{children}</LayoutSwitcher>
       </body>
     </html>
   );
