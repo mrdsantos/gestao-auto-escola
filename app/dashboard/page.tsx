ed8f2fac7e6c2b6ecce5fff19b4df42504589d9b
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2025-01-14 20:40:51 -0300

    alteração de texto da home do dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function DashboardPage() {
     return (
       <div className="border border-black bg-gray-300">
         <h1>Bem-vindo ao Dashboard</h1>
-        <p>Esta é a página principal do dashboard.</p>
+        <p>Esta é a página principalsdajhduisahduisauidhqwuehqwklçelçasmdasklçmdikowqaeiowqneiohqw do dashboard.</p>
       </div>
     );
   }
